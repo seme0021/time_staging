88070da9ba2f60def14cc9837a992df1de368f10
Mikhail Semeniuk <seme0021@gmail.com>
2011-12-03 15:46:20 -0800

    fix

--- unix/mysql2csv.sh
+++ unix/mysql2csv.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 dir=/home/mikhail/work/proj/time/raw
 outfile=$1
+mysql_pwd=$2
 
-mysql -u root -p'hophiper1!' -D time_raw -e"select id,substring(place_url,locate('.com/',place_url)+4) as url,place from fb_places_raw where city='Santa Monica';" | sed 's/\t/,/g' | sed '1d' > $dir/$outfile
+mysql -u root -p"$mysql_pwd" -D time_raw -e"select id,substring(place_url,locate('.com/',place_url)+4) as url,place from fb_places_raw where city='Santa Monica';" | sed 's/\t/,/g' | sed '1d' > $dir/$outfile
