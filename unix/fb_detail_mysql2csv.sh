b5376076f1848afd18b667a5b248a47b88472ba2
Mikhail Semeniuk <seme0021@gmail.com>
2011-12-03 16:16:53 -0800

    replace comma in src_name with dash

--- unix/fb_detail_mysql2csv.sh
+++ unix/fb_detail_mysql2csv.sh
@@ -4,4 +4,4 @@ dir=/home/mikhail/work/proj/time/raw
 outfile=$1
 mysql_pwd=$2
 
-mysql -u root -p"$mysql_pwd" -D time_raw -e"select distinct src_id,src_place,case when instr(url1,'yelp')>1 then substring(url1,locate('.com/',url1)+4) when instr(url2,'yelp')>0 then substring(url2,locate('.com/',url2)+4) when instr(url3,'yelp')>0 then substring(url3,locate('.com/',url3)+4) else null end as url from fb_detail_raw;" | sed 's/\t/,/g' | sed '1d' > $dir/$outfile
+mysql -u root -p"$mysql_pwd" -D time_raw -e"select distinct src_id,replace(src_place,','-'),case when instr(url1,'yelp')>1 then substring(url1,locate('.com/',url1)+4) when instr(url2,'yelp')>0 then substring(url2,locate('.com/',url2)+4) when instr(url3,'yelp')>0 then substring(url3,locate('.com/',url3)+4) else null end as url from fb_detail_raw;" | sed 's/\t/,/g' | sed '1d' > $dir/$outfile
