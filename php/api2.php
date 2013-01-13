35ca3c6d8d68f9c2f4bee6e7b4b12c216c8b56e7
Mikhail Semeniuk <seme0021@gmail.com>
2013-01-12 21:15:22 -0800

    Update php/api2.php

--- php/api2.php
+++ php/api2.php
@@ -1,5 +1,5 @@
 <?php
-//mysql_pconnect("localhost","root","hophiper1!") or die("Could not connect");
+//mysql_pconnect("localhost",user,pass) or die("Could not connect");
 //mysql_select_db("time_raw") or die(mysql_error());
 $users = "mikhail";
 echo '{' . $users . '}';;
